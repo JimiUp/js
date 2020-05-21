c1a11387f77a72e85c03db74cfc82b07c29f688f
JimiUp <36g@msn.com>
2020-05-21 16:55:22 +0800

    xueli.js

---
+++ xueli.js
@@ -0,0 +1,37 @@
+/*
+app下载地址：https://t.cn/A6htR2an
+
+雪梨解锁会员 作者:几米
+
+MITM = m.pearkin.com
+
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/api/Movie/WatchMovie';
+
+const checkvip = '/api/Account/CheckVip';
+
+const vipinfo = '/api/account/IndexDetail';
+
+if (url.indexOf(vip) != -1) {
+	obj["canWath"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(checkvip) != -1) {
+	obj["data"] = "1";
+   obj["value"] = "true";
+	body = JSON.stringify(obj);
+ }
+if (url.indexOf(vipinfo) != -1) {
+	obj["nickName"] = "几米";
+   obj["vipLevel"] = "101";
+   obj["vipEndTime"] = "2222-02-22";
+   obj["cartoonVip"] = "true";
+	body = JSON.stringify(obj);
+ }
+$done({body});
